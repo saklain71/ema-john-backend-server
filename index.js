73c355ddeaca4b23d301d0852fe3816d5b09df5c
Saklain Mustak <saklainmustak@sakmac.local>
2022-05-13 16:36:49 +0600

    added

--- index.js
+++ index.js
@@ -11,9 +11,8 @@ app.use(express.json());
 
 // mongodb connect
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.tpb52.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
-//console.log(uri);
+//console.log(uri)
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-
 async function run(){
     try{
         await client.connect();
