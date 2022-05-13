a02869b92f2c80293102e5bee62211175d7feb39
Saklain Mustak <saklainmustak@sakmac.local>
2022-05-13 16:24:23 +0600

    added comment

--- index.js
+++ index.js
@@ -5,13 +5,11 @@ const port = process.env.PORT || 5000;
 require('dotenv').config();
 const app = express();
 const { MongoClient, ServerApiVersion } = require('mongodb');
-
 //middlewear
 app.use(cors());
 app.use(express.json());
 
-
-// mongodb
+// mongodb connect
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.tpb52.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 //console.log(uri);
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
@@ -85,5 +83,5 @@ app.get('/', (req, res) => {
 })
 
 app.listen(port, () => {
-    console.log(`Ema John running port : ${port}`)
-})
+    console.log(`Ema John running port : ${port}`);
+});
